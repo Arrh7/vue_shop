e6b810203e2da82c17918f9bc2f7070f397fd01c
油炸小丸子 <744654703@qq.com>
2020-05-22 23:50:09 +0800

    登录功能

--- src/main.js
+++ src/main.js
@@ -2,7 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
-
+import './assets/fonts/iconfont.css'
+// 全局样式表
+import './assets/css/global.css'
+// 全局配置axios
+import axios from 'axios'
+Vue.prototype.$http = axios
+// 配置请求根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 Vue.config.productionTip = false
 
 new Vue({
