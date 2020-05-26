ce0d915078c25c525df14305a57c88f91500c122
油炸小丸子 <744654703@qq.com>
2020-05-26 18:59:35 +0800

    完成用户列表

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -67,7 +67,7 @@ export default {
         // 1.将登录成功之后的token，保存到客户端的sessionStorage中
         // 2.项目中除了登录之外的其他API接口，必须在登录之后才能访问
         // 3.token只应在当前的网站打开期间生效，所以将token保存在sessionStorage中
-        console.log(res)
+        // console.log(res)
         window.sessionStorage.setItem('token', res.data.token)
         // 4.通过编程式导航跳转到后台主页，路由地址是/home
         this.$router.push('/home')
--- src/main.js
+++ src/main.js
@@ -10,6 +10,13 @@ import axios from 'axios'
 Vue.prototype.$http = axios
 // 配置请求根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// request请求的拦截器
+axios.interceptors.request.use(config => {
+  console.log(config)
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 在最后必须return config
+  return config
+})
 Vue.config.productionTip = false
 
 new Vue({
